3a21e05d8acdbef491041b73857eaf915695c0e7
Zach Phillips <zjohnny94@gmail.com>
2017-10-31 14:14:23 -0500

    Change Point method to use a public double.

--- LoggingKata/Point.cs
+++ LoggingKata/Point.cs
@@ -2,7 +2,7 @@
 {
     public struct Point
     {
-        decimal Longitude { get; set; }
-        decimal Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
     }
 }
