68239cb3aea3c79001a88dd9dc7fa46f04206883
Zach Phillips <zjohnny94@gmail.com>
2017-10-31 14:20:33 -0500

    Updated Parser to parse csv file and create location points for Taco Bells.

--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using log4net;
+using System;
 
 namespace LoggingKata
 {
@@ -17,10 +18,45 @@ namespace LoggingKata
         private static readonly ILog Logger =
             LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        public ITrackable Parse(string line)
+        public string Name { get; private set; }
+        public Point Location { get; private set; }
+
+        public ITrackable Parse(string row)
         {
-            //DO not fail if one record parsing fails, return null
-            return null; //TODO Implement
+            var cells = row.Split(',');
+
+            if (cells.Length < 3)
+            {
+                Logger.Error("Must have at least 3 elements to parse into ITrackable");
+                return null;
+            }
+
+            double lon = 0;
+            double lat = 0;
+
+            try
+            {
+                Logger.Debug("Attempting to parse Longitude");
+                lon = double.Parse(cells[0]);
+
+                Logger.Debug("Attempting to parse Latitude");
+                lat = double.Parse(cells[1]);
+            }
+            catch (Exception e)
+            {
+                Logger.Error("Failed to parse the location", e);
+                return null;
+            }
+
+            var tacoBell = new TacoBell()
+            {
+                Name = cells[2],
+                Location = new Point { Longitude = lon, Latitude = lat }
+            };
+
+            Logger.Info("Created a new TacoBell");
+
+            return tacoBell;
         }
     }
 }
